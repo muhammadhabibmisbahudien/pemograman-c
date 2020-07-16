ba4e3e76ee575d14a2ddb96cafeff878ae8a0a4f
muhammadhabibmisbahudien <68189316+muhammadhabibmisbahudien@users.noreply.github.com>
2020-07-16 21:59:12 +0700

    Create latihan-for5.c

---
+++ latihan-for5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  int i;
+  for (i=1; i<=9; i++) {
+     
+
+  if(i % 2 == 0) {
+    printf("%d adalahh Genap", i);
+  } else {
+    printf("%d adalah Ganjil", i);
+
+  }
+  printf("\n");
+  }
+
+  return 0;
+}
