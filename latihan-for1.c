7c191d592bc362a26d9f4ebc21e80a1f14353172
muhammadhabibmisbahudien <68189316+muhammadhabibmisbahudien@users.noreply.github.com>
2020-07-16 21:22:13 +0700

    Create latihan-for1.c

---
+++ latihan-for1.c
@@ -0,0 +1,15 @@
+/* File: for.c */
+/* Penulis : Bu Dengklek, email inge@bebras.or.id */
+/* Deskripsi :                                                   
+/* print “Saya senang” sebanyak 3 kali */
+#include<stdio.h>
+int main ()
+{
+/* Kamus : */
+ int i;
+ /* Algoritma */
+ for (i=1; i<=1000; i++) {
+     printf ("%d Saya senang belajar bahasa C \n", i);
+ }
+  return 0;
+}
