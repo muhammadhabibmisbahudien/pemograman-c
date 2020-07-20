95be79b5875c5e0e8ac8dca0bb9a1919c1e906a6
muhammadhabibmisbahudien <68189316+muhammadhabibmisbahudien@users.noreply.github.com>
2020-07-20 18:40:24 +0700

    Create post-2.c

---
+++ POST-TES/post-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Jution Candra Kirana\n");
+    printf("Natar, 3 Juli 1995\n");
+    printf("Bandar Lampung\n");
+    printf("membaca buku");
+    return 0;
+}
